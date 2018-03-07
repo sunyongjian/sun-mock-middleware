ccb8835c99f88037a126d9bbf652ff22459c0c43
sunyongjian <704928775@qq.com>
2018-03-07 15:08:20 +0800

    feat: pub

--- test/mock-middleware.js
+++ test/mock-middleware.js
@@ -1,25 +1,6 @@
 const test = require('ava');
 const request = require('request');
-const express = require('express');
 const path = require('path');
-const SunMockMiddleware = require('../src/index');
-
-// const app = express();
-// const ROOT_DIR = path.resolve(__dirname, '../');
-
-// app.use('/api', SunMockMiddleware({
-//   pathMap: {
-//     // example
-//     '/a/b/c/example': '/example',
-//   },
-//   mockDir: `${ROOT_DIR}/mock`,
-// }));
-
-// app.listen(3333, () => {
-//   console.log('\napp listening on port %s', 3333);
-// });
-
-
 
 
 test.cb('js', t => {
